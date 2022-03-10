347098f8cddfe17707d32e8497a34d512eb2dde6
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2022-03-10 18:03:15 -0500

    Installed MySQL connector and made the connection in the settings.py. Made the initial migration that Django makes the databases.

--- ABC_Project/ABC_Project/settings.py
+++ ABC_Project/ABC_Project/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'ABC_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'abc_project',
+        'USER': 'root',
+        'PASSWORD': 'CharlottE2010!!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
     }
 }
 
